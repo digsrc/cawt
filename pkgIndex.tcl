c8cd1dcb53c539e3e10d7159175d14c2ed6cf8b9
obermeier <obermeier@0cda9279-68ae-45e5-86e6-b65c54811069>
2014-12-09 21:54:52 +0000

    Increased Word and CAWT version to 1.2.0 due to some incompatible changes. Increased Excel version to 1.0.8.

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -13,7 +13,7 @@ proc __cawtSourcePkgs { dir } {
         set retVal [catch {package require $pkg} ::__cawtPkgInfo($pkg,version)]
         set ::__cawtPkgInfo($pkg,avail) [expr !$retVal]
     }
-    package provide cawt 1.1.0
+    package provide cawt 1.2.0
 }
 
-package ifneeded cawt 1.1.0 "[list __cawtSourcePkgs $dir]"
+package ifneeded cawt 1.2.0 "[list __cawtSourcePkgs $dir]"

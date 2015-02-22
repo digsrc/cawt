ce33da26ac70adc4d513eaa49d31de5a26a3d760
obermeier <obermeier@0cda9279-68ae-45e5-86e6-b65c54811069>
2015-02-22 10:24:54 +0000

    New implementation of InsertImage based on Shapes.AddPicture instead of Pictures.Insert (the later embedded images till Excel 2007, since Excel 2010 the images are just linked). Added enhanced tests for inserting modes and scaling in Excel-20_ImgUtil.tcl. Moved tests for displaying images as cell background colors into separate test script Excel-21_ImgCell.tcl.

--- TestPrograms/Excel-20_ImgUtil.tcl
+++ TestPrograms/Excel-20_ImgUtil.tcl
@@ -5,15 +5,14 @@
 
 source "SetTestPathes.tcl"
 package require cawt
-package require Tk
 
-set testImg [file join [pwd] "testIn/wish.gif"]
+set squareImg    [file join [pwd] "testIn/Square.gif"]
+set landscapeImg [file join [pwd] "testIn/Landscape.gif"]
+set portraitImg  [file join [pwd] "testIn/Portrait.gif"]
 
 # Open Excel, show the application window and create a workbook.
 set appId [::Excel::Open true]
 set workbookId [::Excel::AddWorkbook $appId]
-
-set worksheetId1 [::Excel::AddWorksheet $workbookId "Image"]
 
 # Delete Excel file from previous test run.
 file mkdir testOut
@@ -21,83 +20,37 @@ set xlsFile [file join [pwd] "testOut" "Excel-20_ImgUtil"]
 append xlsFile [::Excel::GetExtString $appId]
 file delete -force $xlsFile
 
-frame .f1
-frame .f2
-pack .f1 .f2 -side left
+puts "Inserting images of different sizes ..."
+set worksheetId1 [::Excel::AddWorksheet $workbookId "Sizes"]
+::Excel::SetCellValue $worksheetId1 1 1 "Square"
+::Excel::SetCellValue $worksheetId1 1 5 "Landscape"
+::Excel::SetCellValue $worksheetId1 1 9 "Portrait"
+::Excel::InsertImage $worksheetId1 $squareImg    2 1
+::Excel::InsertImage $worksheetId1 $landscapeImg 2 5
+::Excel::InsertImage $worksheetId1 $portraitImg  2 9
 
-pack [label .f1.l] [label .f1.t] -side top
-pack [label .f2.l] [label .f2.t] -side top
+puts "Inserting images with different modes ..."
+set worksheetId2 [::Excel::AddWorksheet $workbookId "Modes"]
+::Excel::SetCellValue $worksheetId2 1 1 "Linked"
+::Excel::SetCellValue $worksheetId2 1 5 "Embedded"
+::Excel::SetCellValue $worksheetId2 1 9 "Linked and Embedded"
+::Excel::InsertImage $worksheetId2 $squareImg  2 1  true  false
+::Excel::InsertImage $worksheetId2 $squareImg  2 5  false true
+::Excel::InsertImage $worksheetId2 $squareImg  2 9  true  true
 
-# Test inserting and scaling an image into a worksheet.
-puts "Insert an image with Excel ..."
-set picId [::Excel::InsertImage $worksheetId1 $testImg 1 1]
-::Excel::ScaleImage $picId 2 2.5
+set catchVal [ catch { ::Excel::InsertImage $worksheetId2 $squareImg 2 13 false false } retVal]
+if { $catchVal } {
+    puts "Error successfully caught: $retVal"
+}
 
-set phImg [image create photo -file $testImg]
-set w [image width $phImg]
-set h [image height $phImg]
-set numPix [expr { $w * $h }]
-
-.f1.l configure -image $phImg
-.f1.t configure -text  "Original image (Size: $w x $h)"
-update
-
-puts "Add an image as cell background colors (at specific position) ..."
-set worksheetId2 [::Excel::AddWorksheet $workbookId "ImageParam"]
-::Excel::UseImgTransparency false
-
-set t1 [clock clicks -milliseconds]
-::Excel::ImgToWorksheet $phImg $worksheetId2 10 3  5 1
-set t2 [clock clicks -milliseconds]
-puts "[expr $t2 - $t1] ms to put $numPix pixels (ignoring transparency) into worksheet."
-
-puts "Add an image as cell background colors (without screen update) ..."
-set worksheetId3 [::Excel::AddWorksheet $workbookId "ImageNoUpdate"]
-::Excel::UseImgTransparency false
-
-::Excel::ScreenUpdate $appId false
-set t1 [clock clicks -milliseconds]
-::Excel::ImgToWorksheet $phImg $worksheetId3 10 3  5 1
-set t2 [clock clicks -milliseconds]
-puts "[expr $t2 - $t1] ms to put $numPix pixels (ignoring transparency) into worksheet."
-::Excel::ScreenUpdate $appId true
-
-puts "Add an image as cell background colors (Default params) ..."
-set worksheetId4 [::Excel::AddWorksheet $workbookId "ImageDefault"]
-::Excel::UseImgTransparency true
-
-set t1 [clock clicks -milliseconds]
-::Excel::ImgToWorksheet $phImg $worksheetId4
-set t2 [clock clicks -milliseconds]
-puts "[expr $t2 - $t1] ms to put $numPix pixels (using transparency) into worksheet."
-
-puts "Paint a cross ..."
-set rangeId [::Excel::SelectRangeByIndex $worksheetId4 [expr $h/2] 1 [expr $h/2 +1] $w]
-::Excel::SetRangeFillColor $rangeId 255 0 255
-
-set rangeId [::Excel::SelectRangeByIndex $worksheetId4 1 [expr $w/2] $h [expr $w/2 +1]]
-
-::Excel::SetRangeFillColor $rangeId 255 0 255
-
-puts "Export changed image into a Tk photo ..."
-set t1 [clock clicks -milliseconds]
-set phImgPainted [::Excel::WorksheetToImg $worksheetId4 1 1 $h $w]
-set t2 [clock clicks -milliseconds]
-puts "[expr $t2 - $t1] ms to get $numPix pixels from worksheet."
-
-set wp [image width $phImgPainted]
-set hp [image height $phImgPainted]
-
-.f2.l configure -image $phImgPainted
-.f2.t configure -text  "Painted image (Size: $wp x $hp)"
-update
-
-::Cawt::CheckNumber $w $wp "Width of images"
-::Cawt::CheckNumber $h $hp "Height of images"
-
-set imgFile [file join [pwd] "testOut" "Excel-20_ImgUtil.gif"]
-puts "Saving painted images: $imgFile"
-$phImgPainted write $imgFile -format "GIF"
+puts "Inserting and scaling images ..."
+set worksheetId3 [::Excel::AddWorksheet $workbookId "Scaling"]
+::Excel::SetCellValue $worksheetId3 1 1 "Landscape scaled to Square"
+::Excel::SetCellValue $worksheetId3 1 5 "Portrait scaled to Square"
+set shapeId1 [::Excel::InsertImage $worksheetId3 $landscapeImg 2 1]
+::Excel::ScaleImage $shapeId1 1 2
+set shapeId2 [::Excel::InsertImage $worksheetId3 $portraitImg 2 5]
+::Excel::ScaleImage $shapeId2 2 1
 
 puts "Saving as Excel file: $xlsFile"
 ::Excel::SaveAs $workbookId $xlsFile "" false

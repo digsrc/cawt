e51d9e6d9229ac1cb297764baf8e231e3ed21996
obermeier <obermeier@0cda9279-68ae-45e5-86e6-b65c54811069>
2015-03-13 22:21:49 +0000

    Major improvement of Excel worksheet export. Use hand-made Excel test file for HTML export test script Excel-22_Html.tcl.

--- TestPrograms/Excel-22_Html.tcl
+++ TestPrograms/Excel-22_Html.tcl
@@ -1,4 +1,4 @@
-# Test CawtExcel procedures to exchange data between Excel and Tablelist.
+# Test CawtExcel procedures to export Excel data to a HTML file.
 #
 # Copyright: 2007-2015 Paul Obermeier (obermeier@poSoft.de)
 # Distributed under BSD license.
@@ -6,46 +6,29 @@
 source "SetTestPathes.tcl"
 package require cawt
 
-
 # Open new instance of Excel and add a workbook.
 set appId [Excel OpenNew]
+
+set xlsFile [file join [pwd] "testIn" "SampleTable.xls"]
 
 set outFileHtml1 [file join [pwd] "testOut" "Excel-22_Html1.html"]
 set outFileHtml2 [file join [pwd] "testOut" "Excel-22_Html2.html"]
 
-# Delete Excel file from previous test run.
+# Create testOut directory, if it does not yet exist.
 file mkdir testOut
-set xlsFile [file join [pwd] "testOut" "Excel-22_Html"]
-append xlsFile [Excel GetExtString $appId]
-file delete -force $xlsFile
 
-# Create an Excel file with some test data.
-set workbookId [Excel AddWorkbook $appId]
-set headerList { "Col-1" "Col-2" "Col-3" "Col-4" }
-set dataList {
-    {"1" "2" "3" "None"}
-    {"1.1" "1.2" "1.3" "Dot"}
-    {"1,1" "1,2" "1,3" "Comma"}
-    {"1|1" "1|2" "1|3" "Pipe"}
-    {"1;1" "1;2" "1;3" "Semicolon"}
-}
-
-set worksheetId [Excel AddWorksheet $workbookId "HtmlTest"]
-Excel SetHeaderRow $worksheetId $headerList
-Excel SetMatrixValues $worksheetId $dataList 2
-
-Excel SetRangeFillColor [Excel GetCellIdByIndex $worksheetId 1 1] 255   0 0
-Excel SetRangeFillColor [Excel SelectRangeByIndex $worksheetId 5 1 5 4]   0 255 0
+# Open the Excel file with a sample table.
+set workbookId [Excel OpenWorkbook $appId $xlsFile true]
+set worksheetId [Excel GetWorksheetIdByName $workbookId "SampleTable"]
 
 puts "Copy worksheet to HTML file $outFileHtml1"
-Excel WorksheetToHtmlFile $worksheetId $outFileHtml1 true
+Excel WorksheetToHtmlFile $worksheetId $outFileHtml1
 
-Excel SaveAs $workbookId $xlsFile
 Excel Close $workbookId
 Excel Quit $appId
 
 puts "Copy Excel file to HTML file $outFileHtml2"
-Excel ExcelFileToHtmlFile $xlsFile $outFileHtml2 "HtmlTest" true true
+Excel ExcelFileToHtmlFile $xlsFile $outFileHtml2 "SampleTable" true
 
 Cawt PrintNumComObjects
 

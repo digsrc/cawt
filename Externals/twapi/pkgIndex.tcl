8aab66210c5999cbbec448d016c62e77168a6ec8
obermeier <obermeier@0cda9279-68ae-45e5-86e6-b65c54811069>
2015-03-27 23:40:23 +0000

    Updated Twapi to version 4.1.27.

--- Externals/twapi/pkgIndex.tcl
+++ Externals/twapi/pkgIndex.tcl
@@ -72,75 +72,75 @@ proc twapi::package_setup {dir pkg version type {file {}} {commands {}}} {
 
 # The build process will append package ifneeded commands below
 # to create an appropriate pkgIndex.tcl file for included modules
-package ifneeded twapi_base 4.0.61 [list twapi::package_setup $dir twapi_base 4.0.61 load twapi_base {}]
-package ifneeded metoo 4.0.61 [list twapi::package_setup $dir metoo 4.0.61 source {} {}]
-package ifneeded twapi_com 4.0.61 [list twapi::package_setup $dir twapi_com 4.0.61 load {} {}]
-package ifneeded twapi_msi 4.0.61 [list twapi::package_setup $dir twapi_msi 4.0.61 source {} {}]
-package ifneeded twapi_power 4.0.61 [list twapi::package_setup $dir twapi_power 4.0.61 source {} {}]
-package ifneeded twapi_printer 4.0.61 [list twapi::package_setup $dir twapi_printer 4.0.61 source {} {}]
-package ifneeded twapi_synch 4.0.61 [list twapi::package_setup $dir twapi_synch 4.0.61 source {} {}]
-package ifneeded twapi_security 4.0.61 [list twapi::package_setup $dir twapi_security 4.0.61 load {} {}]
-package ifneeded twapi_account 4.0.61 [list twapi::package_setup $dir twapi_account 4.0.61 load {} {}]
-package ifneeded twapi_apputil 4.0.61 [list twapi::package_setup $dir twapi_apputil 4.0.61 load {} {}]
-package ifneeded twapi_clipboard 4.0.61 [list twapi::package_setup $dir twapi_clipboard 4.0.61 load {} {}]
-package ifneeded twapi_console 4.0.61 [list twapi::package_setup $dir twapi_console 4.0.61 load {} {}]
-package ifneeded twapi_crypto 4.0.61 [list twapi::package_setup $dir twapi_crypto 4.0.61 load {} {}]
-package ifneeded twapi_device 4.0.61 [list twapi::package_setup $dir twapi_device 4.0.61 load {} {}]
-package ifneeded twapi_etw 4.0.61 [list twapi::package_setup $dir twapi_etw 4.0.61 load {} {}]
-package ifneeded twapi_eventlog 4.0.61 [list twapi::package_setup $dir twapi_eventlog 4.0.61 load {} {}]
-package ifneeded twapi_mstask 4.0.61 [list twapi::package_setup $dir twapi_mstask 4.0.61 load {} {}]
-package ifneeded twapi_multimedia 4.0.61 [list twapi::package_setup $dir twapi_multimedia 4.0.61 load {} {}]
-package ifneeded twapi_namedpipe 4.0.61 [list twapi::package_setup $dir twapi_namedpipe 4.0.61 load {} {}]
-package ifneeded twapi_network 4.0.61 [list twapi::package_setup $dir twapi_network 4.0.61 load {} {}]
-package ifneeded twapi_nls 4.0.61 [list twapi::package_setup $dir twapi_nls 4.0.61 load {} {}]
-package ifneeded twapi_os 4.0.61 [list twapi::package_setup $dir twapi_os 4.0.61 load {} {}]
-package ifneeded twapi_pdh 4.0.61 [list twapi::package_setup $dir twapi_pdh 4.0.61 load {} {}]
-package ifneeded twapi_process 4.0.61 [list twapi::package_setup $dir twapi_process 4.0.61 load {} {}]
-package ifneeded twapi_rds 4.0.61 [list twapi::package_setup $dir twapi_rds 4.0.61 load {} {}]
-package ifneeded twapi_resource 4.0.61 [list twapi::package_setup $dir twapi_resource 4.0.61 load {} {}]
-package ifneeded twapi_service 4.0.61 [list twapi::package_setup $dir twapi_service 4.0.61 load {} {}]
-package ifneeded twapi_share 4.0.61 [list twapi::package_setup $dir twapi_share 4.0.61 load {} {}]
-package ifneeded twapi_shell 4.0.61 [list twapi::package_setup $dir twapi_shell 4.0.61 load {} {}]
-package ifneeded twapi_storage 4.0.61 [list twapi::package_setup $dir twapi_storage 4.0.61 load {} {}]
-package ifneeded twapi_ui 4.0.61 [list twapi::package_setup $dir twapi_ui 4.0.61 load {} {}]
-package ifneeded twapi_input 4.0.61 [list twapi::package_setup $dir twapi_input 4.0.61 load {} {}]
-package ifneeded twapi_winsta 4.0.61 [list twapi::package_setup $dir twapi_winsta 4.0.61 load {} {}]
-package ifneeded twapi_wmi 4.0.61 [list twapi::package_setup $dir twapi_wmi 4.0.61 load {} {}]
-package ifneeded twapi 4.0.61 {
-  package require twapi_base 4.0.61
-  package require metoo 4.0.61
-  package require twapi_com 4.0.61
-  package require twapi_msi 4.0.61
-  package require twapi_power 4.0.61
-  package require twapi_printer 4.0.61
-  package require twapi_synch 4.0.61
-  package require twapi_security 4.0.61
-  package require twapi_account 4.0.61
-  package require twapi_apputil 4.0.61
-  package require twapi_clipboard 4.0.61
-  package require twapi_console 4.0.61
-  package require twapi_crypto 4.0.61
-  package require twapi_device 4.0.61
-  package require twapi_etw 4.0.61
-  package require twapi_eventlog 4.0.61
-  package require twapi_mstask 4.0.61
-  package require twapi_multimedia 4.0.61
-  package require twapi_namedpipe 4.0.61
-  package require twapi_network 4.0.61
-  package require twapi_nls 4.0.61
-  package require twapi_os 4.0.61
-  package require twapi_pdh 4.0.61
-  package require twapi_process 4.0.61
-  package require twapi_rds 4.0.61
-  package require twapi_resource 4.0.61
-  package require twapi_service 4.0.61
-  package require twapi_share 4.0.61
-  package require twapi_shell 4.0.61
-  package require twapi_storage 4.0.61
-  package require twapi_ui 4.0.61
-  package require twapi_input 4.0.61
-  package require twapi_winsta 4.0.61
-  package require twapi_wmi 4.0.61
+package ifneeded twapi_base 4.1.27 [list twapi::package_setup $dir twapi_base 4.1.27 load twapi_base {}]
+package ifneeded metoo 4.1.27 [list twapi::package_setup $dir metoo 4.1.27 source {} {}]
+package ifneeded twapi_com 4.1.27 [list twapi::package_setup $dir twapi_com 4.1.27 load {} {}]
+package ifneeded twapi_msi 4.1.27 [list twapi::package_setup $dir twapi_msi 4.1.27 source {} {}]
+package ifneeded twapi_power 4.1.27 [list twapi::package_setup $dir twapi_power 4.1.27 source {} {}]
+package ifneeded twapi_printer 4.1.27 [list twapi::package_setup $dir twapi_printer 4.1.27 source {} {}]
+package ifneeded twapi_synch 4.1.27 [list twapi::package_setup $dir twapi_synch 4.1.27 source {} {}]
+package ifneeded twapi_security 4.1.27 [list twapi::package_setup $dir twapi_security 4.1.27 load {} {}]
+package ifneeded twapi_account 4.1.27 [list twapi::package_setup $dir twapi_account 4.1.27 load {} {}]
+package ifneeded twapi_apputil 4.1.27 [list twapi::package_setup $dir twapi_apputil 4.1.27 load {} {}]
+package ifneeded twapi_clipboard 4.1.27 [list twapi::package_setup $dir twapi_clipboard 4.1.27 load {} {}]
+package ifneeded twapi_console 4.1.27 [list twapi::package_setup $dir twapi_console 4.1.27 load {} {}]
+package ifneeded twapi_crypto 4.1.27 [list twapi::package_setup $dir twapi_crypto 4.1.27 load {} {}]
+package ifneeded twapi_device 4.1.27 [list twapi::package_setup $dir twapi_device 4.1.27 load {} {}]
+package ifneeded twapi_etw 4.1.27 [list twapi::package_setup $dir twapi_etw 4.1.27 load {} {}]
+package ifneeded twapi_eventlog 4.1.27 [list twapi::package_setup $dir twapi_eventlog 4.1.27 load {} {}]
+package ifneeded twapi_mstask 4.1.27 [list twapi::package_setup $dir twapi_mstask 4.1.27 load {} {}]
+package ifneeded twapi_multimedia 4.1.27 [list twapi::package_setup $dir twapi_multimedia 4.1.27 load {} {}]
+package ifneeded twapi_namedpipe 4.1.27 [list twapi::package_setup $dir twapi_namedpipe 4.1.27 load {} {}]
+package ifneeded twapi_network 4.1.27 [list twapi::package_setup $dir twapi_network 4.1.27 load {} {}]
+package ifneeded twapi_nls 4.1.27 [list twapi::package_setup $dir twapi_nls 4.1.27 load {} {}]
+package ifneeded twapi_os 4.1.27 [list twapi::package_setup $dir twapi_os 4.1.27 load {} {}]
+package ifneeded twapi_pdh 4.1.27 [list twapi::package_setup $dir twapi_pdh 4.1.27 load {} {}]
+package ifneeded twapi_process 4.1.27 [list twapi::package_setup $dir twapi_process 4.1.27 load {} {}]
+package ifneeded twapi_rds 4.1.27 [list twapi::package_setup $dir twapi_rds 4.1.27 load {} {}]
+package ifneeded twapi_resource 4.1.27 [list twapi::package_setup $dir twapi_resource 4.1.27 load {} {}]
+package ifneeded twapi_service 4.1.27 [list twapi::package_setup $dir twapi_service 4.1.27 load {} {}]
+package ifneeded twapi_share 4.1.27 [list twapi::package_setup $dir twapi_share 4.1.27 load {} {}]
+package ifneeded twapi_shell 4.1.27 [list twapi::package_setup $dir twapi_shell 4.1.27 load {} {}]
+package ifneeded twapi_storage 4.1.27 [list twapi::package_setup $dir twapi_storage 4.1.27 load {} {}]
+package ifneeded twapi_ui 4.1.27 [list twapi::package_setup $dir twapi_ui 4.1.27 load {} {}]
+package ifneeded twapi_input 4.1.27 [list twapi::package_setup $dir twapi_input 4.1.27 load {} {}]
+package ifneeded twapi_winsta 4.1.27 [list twapi::package_setup $dir twapi_winsta 4.1.27 load {} {}]
+package ifneeded twapi_wmi 4.1.27 [list twapi::package_setup $dir twapi_wmi 4.1.27 load {} {}]
+package ifneeded twapi 4.1.27 {
+  package require twapi_base 4.1.27
+  package require metoo 4.1.27
+  package require twapi_com 4.1.27
+  package require twapi_msi 4.1.27
+  package require twapi_power 4.1.27
+  package require twapi_printer 4.1.27
+  package require twapi_synch 4.1.27
+  package require twapi_security 4.1.27
+  package require twapi_account 4.1.27
+  package require twapi_apputil 4.1.27
+  package require twapi_clipboard 4.1.27
+  package require twapi_console 4.1.27
+  package require twapi_crypto 4.1.27
+  package require twapi_device 4.1.27
+  package require twapi_etw 4.1.27
+  package require twapi_eventlog 4.1.27
+  package require twapi_mstask 4.1.27
+  package require twapi_multimedia 4.1.27
+  package require twapi_namedpipe 4.1.27
+  package require twapi_network 4.1.27
+  package require twapi_nls 4.1.27
+  package require twapi_os 4.1.27
+  package require twapi_pdh 4.1.27
+  package require twapi_process 4.1.27
+  package require twapi_rds 4.1.27
+  package require twapi_resource 4.1.27
+  package require twapi_service 4.1.27
+  package require twapi_share 4.1.27
+  package require twapi_shell 4.1.27
+  package require twapi_storage 4.1.27
+  package require twapi_ui 4.1.27
+  package require twapi_input 4.1.27
+  package require twapi_winsta 4.1.27
+  package require twapi_wmi 4.1.27
 
-  package provide twapi 4.0.61
+  package provide twapi 4.1.27
 }

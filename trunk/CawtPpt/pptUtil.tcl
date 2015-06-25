57cbbf6126b42b58ff0015ca0e7e959fb29ebf78
obermeier <obermeier@0cda9279-68ae-45e5-86e6-b65c54811069>
2015-06-25 20:44:24 +0000

    Do not quit PowerPoint in procedure ExportPptFile. This will shut down all other open presentations.

--- trunk/CawtPpt/pptUtil.tcl
+++ trunk/CawtPpt/pptUtil.tcl
@@ -99,7 +99,6 @@ namespace eval Ppt {
 
         Ppt ExportSlides $actPres $outputDir $outputFileFmt $startIndex $endIndex $imgType $width $height
         Ppt Close $presId
-        Ppt Quit $appId
 
         Cawt Destroy $actWin
         Cawt Destroy $actPres

637d75dc5d7ab2dc98e5326e512cc46683554686
obermeier <obermeier@0cda9279-68ae-45e5-86e6-b65c54811069>
2015-10-31 15:45:14 +0000

    Updated enumerations to include new Office 2016 ones.

--- createConstFiles.bat
+++ createConstFiles.bat
@@ -1,8 +1,14 @@
+rem --- Office 2016 ----
+tclsh createConstFile.tcl "C:/Program Files/Microsoft Office/root/Office16/Excel.exe"  "Excel"   > excelConst.tcl
+tclsh createConstFile.tcl "C:/Program Files/Microsoft Office/root/Office16/msword.olb" "Word"    > wordConst.tcl
+tclsh createConstFile.tcl "C:/Program Files/Microsoft Office/root/Office16/msppt.olb"  "Ppt"     > pptConst.tcl
+tclsh createConstFile.tcl "C:/Program Files/Microsoft Office/root/Office16/msoutl.olb" "Outlook" > outlookConst.tcl
+
 rem --- Office 2013 ----
-tclsh createConstFile.tcl "C:/Program Files/Microsoft Office 15/root/office15/Excel.exe"  "Excel"   > excelConst.tcl
-tclsh createConstFile.tcl "C:/Program Files/Microsoft Office 15/root/office15/msword.olb" "Word"    > wordConst.tcl
-tclsh createConstFile.tcl "C:/Program Files/Microsoft Office 15/root/office15/msppt.olb"  "Ppt"     > pptConst.tcl
-tclsh createConstFile.tcl "C:/Program Files/Microsoft Office 15/root/office15/msoutl.olb" "Outlook" > outlookConst.tcl
+rem tclsh createConstFile.tcl "C:/Program Files/Microsoft Office 15/root/office15/Excel.exe"  "Excel"   > excelConst.tcl
+rem tclsh createConstFile.tcl "C:/Program Files/Microsoft Office 15/root/office15/msword.olb" "Word"    > wordConst.tcl
+rem tclsh createConstFile.tcl "C:/Program Files/Microsoft Office 15/root/office15/msppt.olb"  "Ppt"     > pptConst.tcl
+rem tclsh createConstFile.tcl "C:/Program Files/Microsoft Office 15/root/office15/msoutl.olb" "Outlook" > outlookConst.tcl
 
 rem --- Office 2010 ----
 rem tclsh createConstFile.tcl "C:/Program Files (x86)/Microsoft Office/Office14/Excel.exe"  "Excel"   > excelConst.tcl
